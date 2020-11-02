6c8366226532b09032423d03423ae482370c8b93
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-11-02 10:23:14 +0100

    Refactoring

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -3,8 +3,8 @@ using System.Threading;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Configuration;
 using System.Collections.Generic;
+using static System.Console;
 
 namespace Server
 {
@@ -13,20 +13,22 @@ namespace Server
         private static TcpListener _listener;
         private const int Limit = 5; //5 concurrent clients
 
-        private static readonly Dictionary<string, string> Employees = new Dictionary<string, string>
-        {
-            { "jane", "manager" },
-            { "jim", "clerk" },
-            { "jack", "salesman" },
-            { "john", "steno" }
-        };
+        private static readonly Dictionary<string, string> Employees =
+            new Dictionary<string, string>
+            {
+                {"jane", "manager"},
+                {"jim", "clerk"},
+                {"jack", "salesman"},
+                {"john", "steno"}
+            };
+
         public static void Main()
         {
             var localAddr = IPAddress.Parse("127.0.0.1");
             const int port = 2055;
             _listener = new TcpListener(localAddr, port);
             _listener.Start();
-            Console.WriteLine($"Server mounted, listening to port {port}");
+            WriteLine($"Server mounted, listening to port {port}");
             for (var i = 0; i < Limit; i++)
             {
                 var t = new Thread(new ThreadStart(Service));
@@ -38,17 +40,14 @@ namespace Server
         {
             while (true)
             {
-                Socket soc = _listener.AcceptSocket();
-                Console.WriteLine("Connected: {0}",
-                                         soc.RemoteEndPoint);
+                var soc = _listener.AcceptSocket();
+                WriteLine("Connected: {0}", soc.RemoteEndPoint);
                 try
                 {
                     Stream s = new NetworkStream(soc);
-                    StreamReader sr = new StreamReader(s);
-                    StreamWriter sw = new StreamWriter(s);
-                    sw.AutoFlush = true; // enable automatic flushing
-                    sw.WriteLine("{0} Employees available",
-                          Employees.Count);
+                    var sr = new StreamReader(s);
+                    var sw = new StreamWriter(s) {AutoFlush = true};
+                    sw.WriteLine("{0} Employees available", Employees.Count);
                     while (true)
                     {
                         var name = sr.ReadLine();
@@ -56,14 +55,15 @@ namespace Server
                         var job = Employees[name] ?? "No such employee";
                         sw.WriteLine(job);
                     }
+
                     s.Close();
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    WriteLine(e.Message);
                 }
-                Console.WriteLine("Disconnected: {0}",
-                                        soc.RemoteEndPoint);
+
+                WriteLine("Disconnected: {0}", soc.RemoteEndPoint);
                 soc.Close();
             }
         }
